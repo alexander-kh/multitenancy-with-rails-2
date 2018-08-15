c728457be2933fbb3b8a22b6d8cd3d697cdaac3d
Alexander Khlipun <alex.khlipun@icloud.com>
2018-08-15 20:16:07 +0300

    Add Stripe plan fetcher

---
+++ lib/stripe_plan_fetcher.rb
@@ -0,0 +1,18 @@
+class StripePlanFetcher
+  def self.store_locally
+    Stripe::Plan.all.each do |plan|
+      if local_plan = Plan.find_by(stripe_id: plan.id)
+        local_plan.update(
+          name: plan.nickname,
+          amount: plan.amount,
+        )
+      else
+        Plan.create(
+          name: plan.nickname,
+          amount: plan.amount,
+          stripe_id: plan.id
+        )
+      end
+    end
+  end
+end
